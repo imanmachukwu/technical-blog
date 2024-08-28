6f7ec44a0194be00fb9617264832b615b727911d
Imanmachukwu Chukwuemeka <iamimanma@gmail.com>
2024-08-28 11:38:03 +0100

    add: dynamic project page

---
+++ src/pages/project/[uid].js
@@ -0,0 +1,30 @@
+import { createClient } from '@/prismicio'
+import { PrismicText } from '@prismicio/react'
+
+export default function Project({ project }) {
+  return (
+    <div>
+      <h1>
+        <PrismicText field={project.data.title} />
+      </h1>
+    </div>
+  )
+}
+
+export async function getServerSideProps({ params, previewData }) {
+  const client = createClient({ previewData })
+
+  const project = await client.getByUID('project', params.uid)
+
+  if (!project) {
+    return {
+      notFound: true,
+    }
+  }
+
+  return {
+    props: {
+      project,
+    },
+  }
+}
