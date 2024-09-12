71703127026e7a35cb740bb84635c1150f33960e
Imanmachukwu Chukwuemeka <iamimanma@gmail.com>
2024-09-13 00:19:28 +0100

    fix work error

--- dist/static/chunks/webpack.js
+++ dist/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "1cabc4229ff65b64"; }
+/******/ 		__webpack_require__.h = function() { return "9116af382816ee37"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
