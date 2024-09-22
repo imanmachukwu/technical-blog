55f166a2d4236806041bb5fde822a464dcd714ca
Imanmachukwu Chukwuemeka <iamimanma@gmail.com>
2024-09-22 16:32:02 +0100

    fix project styling bug

--- dist/static/chunks/webpack.js
+++ dist/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a0add3a8556a82bd"; }
+/******/ 		__webpack_require__.h = function() { return "82dbc2979d06cf37"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
