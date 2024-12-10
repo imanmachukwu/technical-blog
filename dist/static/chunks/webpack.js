10a36759c915dbea5dba3af779f4ae621cc297aa
Imanmachukwu Chukwuemeka <iamimanma@gmail.com>
2024-12-10 16:13:45 +0100

    add sitemap add robots.txt file change cursor on project hover

--- dist/static/chunks/webpack.js
+++ dist/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e5f837dcd9c01ca6"; }
+/******/ 		__webpack_require__.h = function() { return "6f478c6063029ee7"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
